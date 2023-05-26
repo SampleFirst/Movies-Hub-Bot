cb775a48f0f00d57de7f493be00014a424f675e8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-27 01:01:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -241,22 +241,16 @@ This module only works for my admins
 
 
     LOG_TEXT_G = """#𝐍𝐞𝐰𝐆𝐫𝐨𝐮𝐩
-    
-<b>᚛› 𝐆𝐫𝐨𝐮𝐩 ⪼ {a}(<code>{b}</code>)</b>
-<b>᚛› 𝐆 𝐈𝐃 ⪼ @{c}
-<b>᚛› 𝐓𝐨𝐭𝐚𝐥 𝐌𝐞𝐦𝐛𝐞𝐫𝐬 ⪼ {d}</b>
-<b>᚛› 𝐀𝐝𝐝𝐞𝐝 𝐁𝐲 ⪼ {e}</b>
-
-By {f}
+᚛› 𝐆𝐫𝐨𝐮𝐩 ⪼ = {}(<code>{}</code>)
+᚛› 𝐓𝐨𝐭𝐚𝐥 𝐌𝐞𝐦𝐛𝐞𝐫𝐬 ⪼ = <code>{}</code>
+᚛› 𝐀𝐝𝐝𝐞𝐝 𝐁𝐲 ⪼ - {}
+@iPopkonBot
 """
-    
     LOG_TEXT_P = """#𝐍𝐞𝐰𝐔𝐬𝐞𝐫
-    
-<b>᚛› 𝐈𝐃 - <code>{}</code></b>
-<b>᚛› 𝐍𝐚𝐦𝐞 - {}</b>
-<b>᚛› 𝐔𝐍 - @{}</b>
-
-By @{} """
+᚛› 𝐈𝐃 - <code>{}</code>
+᚛› 𝐍𝐚𝐦𝐞 - {}
+@iPopkonBot
+"""
     
     FILE_MSG = """
 <b>Hai 👋 {} </b>😍
