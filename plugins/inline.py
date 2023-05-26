c09726aff73ba3505bb92c3821a55a63bc224f93
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-26 21:49:12 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -103,7 +103,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
+            InlineKeyboardButton("Letest Upload", url="https://t.me/iPapkornUpdate")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
