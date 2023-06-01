e6ba337064b77ce16f8436f50869499f80cbe2e1
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-01 21:45:20 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -80,9 +80,7 @@ async def send_for_index(bot, message):
         k = await bot.get_messages(chat_id, last_msg_id)
     except:
         return await message.reply('Make Sure That Iam An Admin In The Channel, if channel is private')
-    if k
-
-.empty:
+    if k.empty:
         return await message.reply('This may be group and iam not a admin of the group.')
 
     if message.from_user.id in ADMINS:
