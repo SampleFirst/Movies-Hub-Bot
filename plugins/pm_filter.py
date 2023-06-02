71948e7bccda868ae25775f6a852bfec59d1d402
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-02 14:30:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -611,6 +611,40 @@ async def cb_handler(client: Client, query: CallbackQuery):
         deleted = str(deleted)
         await k.edit_text(text=f"<b>Successfully deleted {deleted} CamRip files.</b>")
 
+    elif query.data == "hdcam":
+        k = await client.send_message(chat_id=query.message.chat.id, text="<b>Deleting HDCams... Please wait...</b>")
+        files, next_offset, total = await get_bad_files(
+                                                  'hdcam',
+                                                  offset=0)
+        deleted = 0
+        for file in files:
+            file_ids = file.file_id
+            result = await Media.collection.delete_one({
+                '_id': file_ids,
+            })
+            if result.deleted_count:
+                logger.info('HDCam File Found ! Successfully deleted from database.')
+            deleted+=1
+        deleted = str(deleted)
+        await k.edit_text(text=f"<b>Successfully deleted {deleted} HDCam files.</b>")
+
+    elif query.data == "s-print":
+        k = await client.send_message(chat_id=query.message.chat.id, text="<b>Deleting S-Prints... Please wait...</b>")
+        files, next_offset, total = await get_bad_files(
+                                                  's-print',
+                                                  offset=0)
+        deleted = 0
+        for file in files:
+            file_ids = file.file_id
+            result = await Media.collection.delete_one({
+                '_id': file_ids,
+            })
+            if result.deleted_count:
+                logger.info('S-Print File Found ! Successfully deleted from database.')
+            deleted+=1
+        deleted = str(deleted)
+        await k.edit_text(text=f"<b>Successfully deleted {deleted} S-Print files.</b>")
+
     elif query.data == "pages":
         await query.answer()
 
