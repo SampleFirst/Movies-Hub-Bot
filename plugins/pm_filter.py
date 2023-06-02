46519462de1a72f62e596ef32062f81b048b4f0e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-02 13:27:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -577,7 +577,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             caption=f_caption,
             protect_content=True if ident == 'checksubp' else False
         )
-elif query.data == "predvd":
+        
+if query.data == "predvd":
     k = await client.send_message(chat_id=query.message.chat.id, text="<b>Deleting PreDVDs... Please wait...</b>")
     files, next_offset, total = await get_bad_files('predvd', offset=0)
     deleted = 0
