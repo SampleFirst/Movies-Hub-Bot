9a183b862b21911909d8fa0000fe250cf651489b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-02 15:36:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1053,26 +1053,43 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
-    
-elif query.data == "rfrsh":
-    await query.answer("Fatching MongoDb Database")
-    buttons = [[
-        InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='stats'),
-        InlineKeyboardButton('ʀᴇғʀᴇsʜ', callback_data='rfrsh')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    total = await Media.count_documents()
-    users = await db.total_users_count()
-    chats = await db.total_chat_count()
-    monsize = await db.get_db_size()
-    free = 536870912 - monsize
-    monsize = get_size(monsize)
-    free = get_size(free)
-    await query.message.edit_text(
-        text=script.STATUS_TXT.format(total, users, chats, monsize, free),
-        reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML
-    )
+    elif query.data == "stats":
+        buttons = [[
+            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start'),
+            InlineKeyboardButton('ʀᴇғʀᴇsʜ', callback_data='rfrsh')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        total = await Media.count_documents()
+        users = await db.total_users_count()
+        chats = await db.total_chat_count()
+        monsize = await db.get_db_size()
+        free = 536870912 - monsize
+        monsize = get_size(monsize)
+        free = get_size(free)
+        await query.message.edit_text(
+            text=script.STATUS_TXT.format(total, users, chats, monsize, free),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+    elif query.data == "rfrsh":
+        await query.answer("Fatching MongoDb Database")
+        buttons = [[
+            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='stats'),
+            InlineKeyboardButton('ʀᴇғʀᴇsʜ', callback_data='rfrsh')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        total = await Media.count_documents()
+        users = await db.total_users_count()
+        chats = await db.total_chat_count()
+        monsize = await db.get_db_size()
+        free = 536870912 - monsize
+        monsize = get_size(monsize)
+        free = get_size(free)
+        await query.message.edit_text(
+            text=script.STATUS_TXT.format(total, users, chats, monsize, free),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
     elif query.data.startswith("setgs"):
         ident, set_type, status, grp_id = query.data.split("#")
         grpid = await active_connection(str(query.from_user.id))
