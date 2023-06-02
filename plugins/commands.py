8ba903763713475ef3317b4c25f523c3700a0fdc
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-02 12:32:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -633,6 +633,9 @@ async def deletemultiplefiles(bot, message):
     btn = [[
             InlineKeyboardButton("Delete PreDVDs", callback_data="predvd"),
             InlineKeyboardButton("Delete CamRips", callback_data="camrip")
+          ],[
+            InlineKeyboardButton("Delete HDCams", callback_data="hdcam"),
+            InlineKeyboardButton("Delete S-Prints", callback_data="sprint")
           ]]
     await message.reply_text(
         text="<b>Select the type of files you want to delete !\n\nThis will delete 100 files from the database for the selected type.</b>",
