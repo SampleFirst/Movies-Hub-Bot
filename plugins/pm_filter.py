77348f7ad32d7feef7112f57448c28f87f7e5f78
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-02 15:33:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1053,25 +1053,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
-    elif query.data == "stats":
-    buttons = [[
-        InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start'),
-        InlineKeyboardButton('ʀᴇғʀᴇsʜ', callback_data='rfrsh')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    total = await Media.count_documents()
-    users = await db.total_users_count()
-    chats = await db.total_chat_count()
-    monsize = await db.get_db_size()
-    free = 536870912 - monsize
-    monsize = get_size(monsize)
-    free = get_size(free)
-    await query.message.edit_text(
-        text=script.STATUS_TXT.format(total, users, chats, monsize, free),
-        reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML
-    )
-
+    
 elif query.data == "rfrsh":
     await query.answer("Fatching MongoDb Database")
     buttons = [[
