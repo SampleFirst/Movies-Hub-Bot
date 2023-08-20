9eb36e0ece0f49ca1686416d5e009117c68e84db
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-20 20:32:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1628,8 +1628,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer(MSG_ALRT)
-
+            await query.answer(MSG_ALRT)
 
 async def auto_filter(client, msg, spoll=False):
     reqstr1 = msg.from_user.id if msg.from_user else 0
