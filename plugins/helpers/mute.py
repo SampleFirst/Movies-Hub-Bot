117d266cd3929d69305b900cdd67367d5b632fda
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-02 23:31:52 +0530

    Update mute.py

--- plugins/helpers/mute.py
+++ plugins/helpers/mute.py
@@ -2,44 +2,50 @@ from pyrogram import Client, filters
 from pyrogram.types import ChatPermissions
 from info import *
 
-
 # Updated extract_user function to extract user details
 def get_user_details(message):
     if message.reply_to_message:
         user = message.reply_to_message.from_user
-        user_id = user.id
-        user_first_name = user.first_name
     else:
-        user_id = message.from_user.id
-        user_first_name = message.from_user.first_name
+        user = message.from_user
+
+    user_id = user.id
+    user_first_name = user.first_name
+
     return user_id, user_first_name
 
+# Use @Client.on_message for both promote and demote commands
 @Client.on_message(filters.command("promote_user") & filters.user(ADMINS))
 async def promote_user(client, message):
-    is_admin = message.from_user and message.from_user.id in ADMINS
+    user_id, user_first_name = get_user_details(message)
 
-    if not is_admin:
-        await message.reply_text(
-            "Admin privileges are required to promote users."
-        )
+    if not user_id:
+        await message.reply_text("You don't seem to be referring to a user.")
         return
 
-    user_id, user_first_name = get_user_details(message)
-    permissions = ChatPermissions(
-        can_change_info=True,
-        can_post_messages=True,
-        can_edit_messages=True,
-        can_delete_messages=True,
-        can_invite_users=True,
-        can_restrict_members=True,
-        can_pin_messages=True,
-        can_promote_members=True 
-    )
     try:
-        await message.chat.promote_member(
-            user_id=user_id,
-            permissions=permissions
+        user_member = await client.get_chat_member(message.chat.id, user_id)
+
+        if user_member.status in ('administrator', 'creator'):
+            await message.reply_text("The user is already an admin or creator.")
+            return
+
+        if user_id == client.me.id:
+            await message.reply_text("I can't promote myself! Get an admin to do it for me.")
+            return
+
+        permissions = ChatPermissions(
+            can_change_info=True,
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_invite_users=True,
+            can_send_polls=True,
+            can_pin_messages=True
         )
+
+        await client.promote_chat_member(message.chat.id, user_id, permissions)
+        
+
         await message.reply_text(
             f"✨ {user_first_name} has been promoted to an admin! 🎉"
         )
@@ -48,32 +54,19 @@ async def promote_user(client, message):
 
 @Client.on_message(filters.command("demote_user") & filters.user(ADMINS))
 async def demote_user(client, message):
-    is_admin = message.from_user and message.from_user.id in ADMINS
+    user_id, user_first_name = get_user_details(message)
 
-    if not is_admin:
-        await message.reply_text(
-            "Admin privileges are required to demote users."
-        )
+    if not user_id:
+        await message.reply_text("You don't seem to be referring to a user.")
         return
 
-    user_id, user_first_name = get_user_details(message)
-    permissions = ChatPermissions(
-        can_change_info=False,
-        can_post_messages=False,
-        can_edit_messages=False,
-        can_delete_messages=False,
-        can_invite_users=False,
-        can_restrict_members=False,
-        can_pin_messages=False,
-        can_promote_members=False
-    )
     try:
-        await message.chat.restrict_member(
+        await client.restrict_chat_member(
+            chat_id=message.chat.id,
             user_id=user_id,
-            permissions=permissions
+            permissions=ChatPermissions(can_send_messages=True)
         )
-        await message.reply_text(
-            f"🔥 {user_first_name} has been demoted to a regular member!"
-        )
+
+        await message.reply_text(f"🔥 {user_first_name} has been demoted to a regular member!")
     except Exception as error:
         await message.reply_text(str(error))
