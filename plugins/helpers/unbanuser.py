91e5374fcb761d2b0dc3de851bac96a3a943b8e6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-03 17:24:14 +0530

    Update unbanuser.py

--- plugins/helpers/unbanuser.py
+++ plugins/helpers/unbanuser.py
@@ -1,14 +1,15 @@
 from pyrogram import Client, filters
 
-@Client.on_message(filters.private & filters.command("unban_user"))
-async def unban_user_pm(_, message):
+
+@Client.on_message(filters.private & filters.command("unban_pm"))
+async def unban_user_pm(client, message):
+    
     try:
-        # Parse the command in the format "/unban chat_id user_id"
-        _, chat_id, user_id = message.text.split()
-        chat_id = int(chat_id)
+        # Parse the command in the format "/unban_pm user_id"
+        _, user_id = message.text.split()
         user_id = int(user_id)
     except ValueError:
-        await message.reply_text("Invalid command format. Please use /unban_user chat_id user_id")
+        await message.reply_text("Invalid command format. Please use /unban_pm user_id")
         return
     
     is_admin = message.from_user and message.from_user.id in ADMINS
@@ -16,9 +17,12 @@ async def unban_user_pm(_, message):
         await message.reply_text("You are not authorized to perform this action.")
         return
     
+    chat_id = -1001912697656  # Replace with the desired chat_id
+    
     try:
-        await Client.get_chat(chat_id).unban_member(user_id=user_id)
+        chat = await client.get_chat(chat_id)
+        await chat.unban_member(user_id=user_id)
     except Exception as error:
-        await message.reply_text(str(error))
+        await message.reply_text(f"An error occurred: {str(error)}")
     else:
         await message.reply_text("User has been unbanned in the specified chat.")
