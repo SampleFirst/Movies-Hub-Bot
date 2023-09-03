0248a0bf70a9c54d801a329c091778f2845e9fc2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-03 22:35:17 +0530

    Update purge.py

--- plugins/purge.py
+++ plugins/purge.py
@@ -1,29 +1,49 @@
 import asyncio
-from pyrogram import Client, filters, enums
-from info import ADMINS
+from pyrogram import Client, filters
+from info import ADMINS  # Assuming you have a file named 'info.py' with the ADMINS list.
 
-@Client.on_message(filters.command("clean") & (filters.group | filters.channel))
-async def clean(client, message):
-    if message.chat.type not in ((enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL)):
+# Define your chat IDs
+chat_ids = [-1001517669863]
+
+@Client.on_message(filters.command("purge") & filters.private)
+async def purge(client, message):
+    if message.chat.id not in chat_ids:
         return
-    is_admin = message.from_user.id in ADMINS
+
+    is_admin = message.from_user and message.from_user.id in ADMINS
     if not is_admin:
         return
 
-    status_message = await message.reply_text("Cleaning up...", quote=True)
+    # Send a status message indicating the purge is in progress
+    status_message = await message.reply_text("Purging...", quote=True)
     await message.delete()
 
-    # Get all message IDs in the chat
-    all_messages = await client.get_history(chat_id=message.chat.id, limit=None)
-    message_ids = [msg.message_id for msg in all_messages]
-
-    # Delete messages in batches of 100
+    message_ids = []
     count_deletions = 0
-    for batch_start in range(0, len(message_ids), 100):
-        batch = message_ids[batch_start : batch_start + 100]
-        await client.delete_messages(chat_id=message.chat.id, message_ids=batch, revoke=True)
-        count_deletions += len(batch)
-    
+
+    if message.reply_to_message:
+        # Loop through message IDs to be deleted
+        for a_message_id in range(message.reply_to_message.message_id, message.message_id):
+            message_ids.append(a_message_id)
+            if len(message_ids) == 100:
+                await client.delete_messages(
+                    chat_id=message.chat.id,
+                    message_ids=message_ids,
+                    revoke=True
+                )
+                count_deletions += len(message_ids)
+                message_ids = []
+
+        # Delete any remaining messages
+        if len(message_ids) > 0:
+            await client.delete_messages(
+                chat_id=message.chat.id,
+                message_ids=message_ids,
+                revoke=True
+            )
+            count_deletions += len(message_ids)
+
+    # Edit the status message to indicate the number of deleted messages
     await status_message.edit_text(f"Deleted {count_deletions} messages")
     await asyncio.sleep(5)
     await status_message.delete()
