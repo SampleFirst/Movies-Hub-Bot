676a9887866754c503f79303e60fc19db9592171
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-03 23:21:47 +0530

    Update and rename purge.py to delete_msg.py

---
+++ plugins/delete_msg.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import re
+
+
+# Define the delete_msg command handler
+@Client.on_message(filters.private & filters.command("delete_message"))
+async def delete_messages_command(client, message: Message):
+    try:
+        # Extract the chat ID and message link from the command
+        command_parts = message.text.split()
+        if len(command_parts) != 3:
+            await message.reply("Invalid command format. Use /delete_msg <channel_message_link> <num_messages>")
+            return
+
+        channel_message_link = command_parts[1]
+        num_messages = int(command_parts[2])
+
+        # Extract the chat ID from the message link using regular expressions
+        match = re.match(r"https://t\.me/.+?/(\d+)", channel_message_link)
+        if not match:
+            await message.reply("Invalid channel message link format.")
+            return
+
+        chat_id = int(match.group(1))
+
+        # Delete the specified number of messages in the chat
+        await client.delete_messages(chat_id, list(range(message.message_id + 1, message.message_id + 1 + num_messages)))
+        await message.reply(f"{num_messages} messages deleted successfully.")
+    except Exception as e:
+        await message.reply(f"An error occurred: {str(e)}")
