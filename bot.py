7ef5f60c04364d50f983269e2814182a2086a014
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-18 17:05:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,25 +76,30 @@ class Bot(Client):
     
     async def send_report_message(self):
         while True:
+            tz = pytz.timezone('Asia/Kolkata')
+            today = date.today()
+            now = datetime.now(tz)
+            formatted_date_1 = now.strftime("%d-%B-%Y")
+            formatted_date_2 = today.strftime("%d %b")
+            time = now.strftime("%H:%M:%S %p")
+
             total_users = await db.total_users_count()
             total_chats = await db.total_chat_count()
             today_users = await db.daily_users_count(today) + 1
             today_chats = await db.daily_chats_count(today) + 1
-            tz = pytz.timezone('Asia/Kolkata')
-            today = date.today()
-            now = datetime.now(tz)
-            time = now.strftime("%H:%M:%S %p")
+
             if now.hour == 23 and now.minute == 59:
                 await self.send_message(
                     chat_id=LOG_CHANNEL, 
                     text=script.REPORT_TXT.format(
-                        a=today,
-                        b=time,
-                        c=total_users, 
-                        d=total_chats,
-                        e=today_users, 
-                        f=today_chats,
-                        g=temp.U_NAME
+                        a=formatted_date_1,
+                        b=formatted_date_2,
+                        c=time,
+                        d=total_users, 
+                        e=total_chats,
+                        f=today_users, 
+                        g=today_chats,
+                        h=temp.U_NAME
                     )
                 )
                 # Sleep for 1 minute to avoid sending multiple messages
