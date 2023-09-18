cf60bd9aec69a6363e90190438a961e9fdeb708f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-09-18 19:42:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,41 +1,75 @@
-# Kanged From @TroJanZheX
-# REDIRECT added https://github.com/Joelkb
+# Standard Library Imports
 import asyncio
-import re
-import ast
+import datetime
+import logging
 import math
 import random
-import datetime
+import re
+import ast
 import shutil
 import psutil
 import time
 
+# Third-Party Library Imports
 from pytz import timezone
-from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from Script import script
-import pyrogram
-from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
-    make_inactive
-from info import ADMINS, AUTH_CHANNEL, FILE_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, NOR_IMG, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
-    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE, SPELL_IMG, MSG_ALRT, FILE_FORWARD, MAIN_CHANNEL, LOG_CHANNEL, PICS, SUPPORT_CHAT_ID
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters, enums, types
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, get_time, humanbytes
-from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
-from database.filters_mdb import (
-    del_all,
-    find_filter,
-    get_filters,
-)
-from database.gfilters_mdb import (
-    find_gfilter,
-    get_gfilters,
-)
-import logging
-from .helper.carbon import make_carbon
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 
+# Database Imports
+from database.connections_mdb import (
+    active_connection,
+    all_connections,
+    delete_connection,
+    if_active,
+    make_active,
+    make_inactive,
+)
+from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
+from database.filters_mdb import del_all, find_filter, get_filters
+from database.gfilters_mdb import find_gfilter, get_gfilters
+from database.users_chats_db import db
+
+# Custom Configuration Imports
+from info import (
+    ADMINS,
+    AUTH_CHANNEL,
+    FILE_CHANNEL,
+    AUTH_USERS,
+    CUSTOM_FILE_CAPTION,
+    NOR_IMG,
+    AUTH_GROUPS,
+    P_TTI_SHOW_OFF,
+    IMDB,
+    SINGLE_BUTTON,
+    SPELL_CHECK_REPLY,
+    IMDB_TEMPLATE,
+    SPELL_IMG,
+    MSG_ALRT,
+    FILE_FORWARD,
+    MAIN_CHANNEL,
+    LOG_CHANNEL,
+    PICS,
+    SUPPORT_CHAT_ID,
+)
+
+# Custom Script and Utility Imports
+from Script import script
+from utils import (
+    get_size,
+    is_subscribed,
+    get_poster,
+    search_gagala,
+    temp,
+    get_settings,
+    save_group_settings,
+    get_shortlink,
+    get_time,
+    humanbytes,
+)
+
+# Set the logging level
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
