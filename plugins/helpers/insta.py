6ffe05842635a9a42a9b1a10cff1b15c958e7aba
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-13 15:16:25 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
-import os
+from bs4 import BeautifulSoup  # Make sure to install BeautifulSoup using pip
 
 @Client.on_message(filters.command("insta"))
 def download_instagram_post(client, message):
@@ -15,29 +15,36 @@ def download_instagram_post(client, message):
         # Download the Instagram post
         try:
             response = requests.get(modified_link)
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Check if the post contains images or videos
-            if "image" in response.headers["content-type"]:
-                file_extension = "jpg"
-            elif "video" in response.headers["content-type"]:
-                file_extension = "mp4"
+            # Check if the post is a video
+            video_tag = soup.find('meta', property='og:video')
+            if video_tag:
+                video_url = video_tag['content']
+                # Download the video
+                video_response = requests.get(video_url)
+                video_file_name = "downloaded_post.mp4"
+                with open(video_file_name, "wb") as video_file:
+                    video_file.write(video_response.content)
+                message.reply_video(video_file_name, caption="Post video downloaded successfully!")
+
+            # Check if the post is an image
             else:
-                message.reply_text("Unsupported content type. Unable to download.")
-                return
+                image_tag = soup.find('meta', property='og:image')
+                if image_tag:
+                    image_url = image_tag['content']
+                    # Download the image
+                    image_response = requests.get(image_url)
+                    image_file_name = "downloaded_post.jpg"
+                    with open(image_file_name, "wb") as image_file:
+                        image_file.write(image_response.content)
+                    message.reply_photo(image_file_name, caption="Post image downloaded successfully!")
 
-            # Save the content to a file
-            file_name = f"downloaded_post.{file_extension}"
-            with open(file_name, "wb") as file:
-                file.write(response.content)
-
-            # Send the downloaded file as a document
-            if file_extension == "jpg":
-                message.reply_photo(photo=file_name, caption="Post downloaded successfully!")
-            elif file_extension == "mp4":
-                message.reply_video(video=file_name, caption="Post downloaded successfully!")
-
-            # Remove the temporary file
-            os.remove(file_name)
+            # Remove the temporary files
+            if 'video_file_name' in locals():
+                os.remove(video_file_name)
+            if 'image_file_name' in locals():
+                os.remove(image_file_name)
 
         except Exception as e:
             message.reply_text(f"Error: {str(e)}")
