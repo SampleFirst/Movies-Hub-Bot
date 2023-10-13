1f40eb5fe855aa198547b045f2a06e398710ae3f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-13 22:14:25 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,64 +1,43 @@
 from pyrogram import Client, filters
 import requests
-import os
-import re
+from bs4 import BeautifulSoup
+from pyrogram.types import InputMediaVideo, InputMediaPhoto
+
+
+
+def get_instagram_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    video_url = soup.find("meta", property="og:video")["content"]
+    image_url = soup.find("meta", property="og:image")["content"]
+
+    return video_url, image_url
+
 
 @Client.on_message(filters.command("insta"))
-async def download_instagram_post(client, message):
-    if len(message.command) > 1:
-        instagram_link = message.command[1]
+def instagram_download(client, message):
+    if len(message.command) == 2:
+        url = message.command[1]
+        video_url, image_url = get_instagram_content(url)
 
-        try:
-            response = requests.get(instagram_link)
+        # Download video and image
+        video_file = f"{url.split('/')[-1]}.mp4"
+        image_file = f"{url.split('/')[-1]}.jpg"
+        with open(video_file, "wb") as video, open(image_file, "wb") as image:
+            video.write(requests.get(video_url).content)
+            image.write(requests.get(image_url).content)
 
-            # Extract post shortcode from the link
-            shortcode_match = re.search(r"/p/([^/?]+)|/reel/([^/?]+)", instagram_link)
-            if shortcode_match:
-                shortcode = shortcode_match.group(1) or shortcode_match.group(2)
+        # Send back the content
+        client.send_media_group(
+            message.chat.id,
+            [
+                InputMediaVideo(video_file, caption="Instagram Reel"),
+                InputMediaPhoto(image_file, caption="Instagram Post"),
+            ],
+        )
 
-                if "/p/" in instagram_link:
-                    file_extension = "jpg"
-                    file_name = f"downloaded_post_{shortcode}.{file_extension}"
+        # Clean up downloaded files
+        os.remove(video_file)
+        os.remove(image_file)
 
-                    # Save the downloaded image
-                    with open(file_name, "wb") as image_file:
-                        image_file.write(response.content)
-
-                    chat_id = message.chat.id
-
-                    # Send the downloaded image with best quality
-                    await client.send_photo(
-                        chat_id=chat_id,
-                        photo=file_name,
-                        caption=f"**ᴛɪᴛʟᴇ :** []()\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
-                    )
-
-                    os.remove(file_name)
-
-                elif "/reel/" in instagram_link:
-                    file_extension = "mp4"
-                    file_name = f"downloaded_reel_{shortcode}.{file_extension}"
-
-                    # Save the downloaded video
-                    with open(file_name, "wb") as video_file:
-                        video_file.write(response.content)
-
-                    chat_id = message.chat.id
-
-                    # Send the downloaded video with best quality
-                    await client.send_video(
-                        chat_id=chat_id,
-                        video=file_name,
-                        caption=f"**ᴛɪᴛʟᴇ :** []()\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
-                    )
-
-                    os.remove(file_name)
-
-                else:
-                    await message.reply_text("Unknown Instagram post type.")
-            else:
-                await message.reply_text("Invalid Instagram link.")
-        except Exception as e:
-            await message.reply_text(f"Error: {str(e)}")
-    else:
-        await message.reply_text("Please provide an Instagram post or reel link with the command.")
