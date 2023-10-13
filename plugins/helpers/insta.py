f118e7fa93bc267daed9770ebdd35394188fa681
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-13 21:12:12 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 import requests
 import os
+import re
 
 @Client.on_message(filters.command("insta"))
 async def download_instagram_post(client, message):
@@ -10,50 +11,54 @@ async def download_instagram_post(client, message):
         try:
             response = requests.get(instagram_link)
 
-            if "/p/" in instagram_link:
-                file_extension = "jpg"
-                file_name = f"downloaded_post.{file_extension}"
+            # Extract post shortcode from the link
+            shortcode_match = re.search(r"/p/([^/?]+)|/reel/([^/?]+)", instagram_link)
+            if shortcode_match:
+                shortcode = shortcode_match.group(1) or shortcode_match.group(2)
 
-                # Save the downloaded image
-                with open(file_name, "wb") as image_file:
-                    image_file.write(response.content)
+                if "/p/" in instagram_link:
+                    file_extension = "jpg"
+                    file_name = f"downloaded_post_{shortcode}.{file_extension}"
 
-                chat_id = message.chat.id
+                    # Save the downloaded image
+                    with open(file_name, "wb") as image_file:
+                        image_file.write(response.content)
 
-                # Send the downloaded image with best quality
-                await client.send_photo(
-                    chat_id=chat_id,
-                    photo=file_name,
-                    caption=f"**ᴛɪᴛʟᴇ :** []()\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
-                    supports_streaming=True,
-                )
+                    chat_id = message.chat.id
 
-                os.remove(file_name)
+                    # Send the downloaded image with best quality
+                    await client.send_photo(
+                        chat_id=chat_id,
+                        photo=file_name,
+                        caption=f"**ᴛɪᴛʟᴇ :** []()\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
+                    )
 
-            elif "/reel/" in instagram_link:
-                file_extension = "mp4"
-                file_name = f"downloaded_reel.{file_extension}"
+                    os.remove(file_name)
 
-                # Save the downloaded video
-                with open(file_name, "wb") as video_file:
-                    video_file.write(response.content)
+                elif "/reel/" in instagram_link:
+                    file_extension = "mp4"
+                    file_name = f"downloaded_reel_{shortcode}.{file_extension}"
 
-                chat_id = message.chat.id
+                    # Save the downloaded video
+                    with open(file_name, "wb") as video_file:
+                        video_file.write(response.content)
 
-                # Send the downloaded video with best quality
-                await client.send_video(
-                    chat_id=chat_id,
-                    video=file_name,
-                    caption=f"**ᴛɪᴛʟᴇ :** []()\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
-                    supports_streaming=True,
-                )
+                    chat_id = message.chat.id
 
-                os.remove(file_name)
+                    # Send the downloaded video with best quality
+                    await client.send_video(
+                        chat_id=chat_id,
+                        video=file_name,
+                        caption=f"**ᴛɪᴛʟᴇ :** []()\n**ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}",
+                    )
 
+                    os.remove(file_name)
+
+                else:
+                    await message.reply_text("Unknown Instagram post type.")
             else:
-                await message.reply_text("Unknown Instagram post type.")
+                await message.reply_text("Invalid Instagram link.")
         except Exception as e:
             await message.reply_text(f"Error: {str(e)}")
     else:
-        await message.reply_text("Please provide an Instagram post link with the command.")
-
+        await message.reply_text("Please provide an Instagram post or reel link with the command.")
