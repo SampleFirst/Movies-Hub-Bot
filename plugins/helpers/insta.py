a7651807dd395001071acb99e471bee0f8a2fd0b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-14 00:41:55 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,41 +1,32 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 from instaloader import Instaloader, Post
 
-@Client.on_message(filters.command("insta"))
-async def instagram_download(client, message):
+# Create an Instaloader instance
+instaloader = Instaloader()
+
+
+# Define a command handler for /sendpost
+@Client.on_message(filters.command("sendpost"))
+def send_post(client, message):
+    # Get the Instagram post link from the command
+    post_link = message.text.split(" ", 1)[1]
+
     try:
-        # Get the Instagram reel link from the command
-        command_parts = message.text.split(" ", 1)
-
-        if len(command_parts) == 2:
-            reel_link = command_parts[1]
-
-            # Ensure it's a valid Instagram Reels link
-            if "instagram.com/reel/" in reel_link:
-                # Download the reel using Instaloader
-                insta_loader = Instaloader()
-
-                # Extracting Post ID from the Reels link
-                post_id = reel_link.split("/")[-1]
-
-                # Download the post
-                post = Post.from_shortcode(insta_loader.context, post_id)
-                insta_loader.download_post(post, target='Reels')
-
-                # Send the downloaded video to the user
-                video_path = f"Reels/{post.date_utc.strftime('%Y%m%d_%H%M%S')}_{post_id}.mp4"
-                caption = f"Instagram Reels Download\nLink: {reel_link}"
-                await client.send_video(
-                    chat_id=message.chat.id,
-                    video=video_path,
-                    caption=caption,
-                )
-            else:
-                await message.reply_text("Please provide a valid Instagram Reels link.")
-        else:
-            await message.reply_text("Please provide a valid Instagram Reels link.")
-
+        # Get post details using Instaloader
+        post = instaloader.load_post_from_url(post_link)
+        
+        # Process the post and send images or videos
+        send_post_media(client, message.chat.id, post)
     except Exception as e:
-        error_message = f"An error occurred: {str(e)}"
-        print(error_message)
-        await message.reply_text(error_message)
+        print(f"Error: {e}")
+        client.send_message(message.chat.id, "Error processing the Instagram post.")
+
+def send_post_media(client: Client, chat_id: int, post: Post):
+    # Send images
+    for index, image_url in enumerate(post.get_sidecar_nodes()):
+        client.send_photo(chat_id, image_url.display_url, caption=f"Image {index + 1}")
+
+    # Send videos
+    if post.is_video:
+        client.send_video(chat_id, post.video_url, caption="Video")
