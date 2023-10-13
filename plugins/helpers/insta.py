2c9413c402665e27cebf8f8891f7110273eeaff7
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-13 14:08:28 +0530

    Create insta.py

---
+++ plugins/helpers/insta.py
@@ -0,0 +1,26 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import requests
+
+@Client.on_message(filters.command("download"))
+def download_instagram_post(client, message):
+    # Get the Instagram post link from the command arguments
+    if len(message.command) > 1:
+        instagram_link = message.command[1]
+
+        # Add "dd" before "instagram"
+        modified_link = instagram_link.replace("://www.instagram.com", "://www.ddinstagram.com")
+
+        # Download the Instagram post
+        try:
+            response = requests.get(modified_link)
+            # Process the response or save the content as needed
+            # For example, you can save it to a file
+            with open("downloaded_post.html", "wb") as file:
+                file.write(response.content)
+            message.reply_text("Post downloaded successfully!")
+        except Exception as e:
+            message.reply_text(f"Error: {str(e)}")
+    else:
+        message.reply_text("Please provide an Instagram post link with the command.")
+
