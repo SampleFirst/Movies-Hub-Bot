5a3a7387f6056f136448b0ca6d30690855fe6107
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-13 18:47:20 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
+import os
 
 @Client.on_message(filters.command("insta"))
 def download_instagram_post(client, message):
@@ -18,22 +19,36 @@ def download_instagram_post(client, message):
             # Check if it's an image or a reel
             if "/p/" in modified_link:
                 # It's a post
-                # Process the response or save the content as needed
-                # For example, you can save it to a file
-                file_name = "downloaded_post.jpg"
+                file_extension = "jpg"
+                file_name = f"downloaded_post.{file_extension}"
+
+                # Save the content to a file
                 with open(file_name, "wb") as file:
                     file.write(response.content)
+
                 # Send the downloaded image as a photo
-                message.reply_photo(photo=file_name, caption="Post downloaded successfully!")
+                with open(file_name, "rb") as photo_file:
+                    message.reply_photo(photo=photo_file, caption="Post downloaded successfully!")
+
+                # Remove the temporary file after sending
+                os.remove(file_name)
+
             elif "/reel/" in modified_link:
                 # It's a reel
-                # Process the response or save the content as needed
-                # For example, you can save it to a file
-                file_name = "downloaded_reel.mp4"
+                file_extension = "mp4"
+                file_name = f"downloaded_reel.{file_extension}"
+
+                # Save the content to a file
                 with open(file_name, "wb") as file:
                     file.write(response.content)
+
                 # Send the downloaded video as a document
-                message.reply_video(video=file_name, caption="Reel downloaded successfully!")
+                with open(file_name, "rb") as video_file:
+                    message.reply_video(video=video_file, caption="Reel downloaded successfully!")
+
+                # Remove the temporary file after sending
+                os.remove(file_name)
+
             else:
                 # Unknown post type
                 message.reply_text("Unknown Instagram post type.")
@@ -41,4 +56,3 @@ def download_instagram_post(client, message):
             message.reply_text(f"Error: {str(e)}")
     else:
         message.reply_text("Please provide an Instagram post link with the command.")
-
