3739e93206ca5bad69a0b05e44c349abe3786d25
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-14 16:15:01 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,11 +1,8 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
 import requests
-from io import BytesIO
-from PIL import Image
 
 @Client.on_message(filters.command("download"))
-def download_instagram_post(client, message):
+async def download_instagram_post(client, message):
     # Get the Instagram post link from the command arguments
     if len(message.command) > 1:
         instagram_link = message.command[1]
@@ -16,23 +13,20 @@ def download_instagram_post(client, message):
         # Download the Instagram post
         try:
             response = requests.get(modified_link)
-
-            # Check if the response contains image data
-            if response.headers.get('content-type', '').startswith('image'):
-                # Process the response and save as an image
-                image = Image.open(BytesIO(response.content))
-                image_file_name = "downloaded_post.jpg"
-                image.save(image_file_name, "JPEG")
-
-                # Send the downloaded image as a document
-                message.reply_document(document=image_file_name, caption="Post downloaded successfully!")
-            else:
-                # Handle the case where the response does not contain image data
-                message.reply_text("Error: The provided link does not seem to point to an image. Check if the link is correct or if the content is accessible.")
+            
+            # Process the response or save the content as needed
+            # For example, you can save it to an "Images" folder
+            file_name = "downloaded_post.jpg"
+            with open(file_name, "wb") as file:
+                file.write(response.content)
+            
+            # Send the downloaded file as an image
+            await client.send_photo(
+                chat_id=message.chat.id,
+                photo=open(file_name, "rb"),
+                caption="Post downloaded successfully!"
+            )
         except Exception as e:
-            # Handle other exceptions
-            message.reply_text(f"Error: {str(e)}")
+            await message.reply_text(f"Error: {str(e)}")
     else:
-        # Handle the case where no Instagram post link is provided
-        message.reply_text("Please provide an Instagram post link with the command.")
-
+        await message.reply_text("Please provide an Instagram post link with the command.")
