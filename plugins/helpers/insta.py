b2fae8254f6ce44b4b9f2d4113f142e11b8e791f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-14 16:49:25 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,9 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
+import aiohttp
 
 @Client.on_message(filters.command("download"))
-def download_instagram_post(client, message):
+async def download_instagram_post(client, message):
     # Get the Instagram post link from the command arguments
     if len(message.command) > 1:
         instagram_link = message.command[1]
@@ -13,18 +14,19 @@ def download_instagram_post(client, message):
 
         # Download the Instagram post
         try:
-            response = requests.get(modified_link)
-            # Process the response or save the content as needed
-            # For example, you can save it to a file
-            file_name = "downloaded_post.html"
-            with open(file_name, "wb") as file:
-                file.write(response.content)
-            # Send the downloaded file as a document
-            await message.reply_photo(
-                photo=file_name, 
-                caption="Post downloaded successfully!"
-            )
+            async with aiohttp.ClientSession() as session:
+                async with session.get(modified_link) as response:
+                    # Process the response or save the content as needed
+                    # For example, you can save it to a file
+                    file_name = "downloaded_post.html"
+                    with open(file_name, "wb") as file:
+                        file.write(await response.read())
+                    # Send the downloaded file as a document
+                    await message.reply_photo(
+                        photo=file_name,
+                        caption="Post downloaded successfully!"
+                    )
         except Exception as e:
-            message.reply_text(f"Error: {str(e)}")
+            await message.reply_text(f"Error: {str(e)}")
     else:
-        message.reply_text("Please provide an Instagram post link with the command.")
+        await message.reply_text("Please provide an Instagram post link with the command.")
