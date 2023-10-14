23edb06e74d3158425560a16737a6cf996ec80d5
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-14 14:49:06 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,34 +1,34 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
-import instaloader
+import requests
 
-
-def download_and_send_media(url, chat_id, client):
-    L = instaloader.Instaloader()
-
+@Client.on_message(filters.command("download"))
+def download_instagram_post(client, message):
     try:
-        post = instaloader.Post.from_shortcode(L.context, url)
-        if post.is_video:
-            video_path = f"{post.owner_username}_{post.shortcode}.mp4"
-            L.download_post(post, target=video_path, post_filter=lambda _: _.is_video)
-            client.send_video(chat_id, video_path)
-        else:
-            image_path = f"{post.owner_username}_{post.shortcode}.jpg"
-            L.download_post(post, target=image_path, post_filter=lambda _: not _.is_video)
-            client.send_photo(chat_id, image_path)
+        # Extracting the original link from the message
+        original_link = message.text.split()[1]
+
+        # Modifying the link for extraction
+        modified_link = original_link.replace("www.instagram.com", "www.ddinstagram.com")
+
+        # Fetch HTML content to extract image URL
+        response = requests.get(modified_link)
+        response.raise_for_status()  # Check for request errors
+
+        # Extract image URL from HTML content using JSON parsing
+        json_data = response.json()
+        image_url = json_data["graphql"]["shortcode_media"]["display_url"]
+
+        # Download the image
+        image_response = requests.get(image_url)
+        image_response.raise_for_status()  # Check for request errors
+
+        # Save the image
+        with open("downloaded_image.jpg", "wb") as image_file:
+            image_file.write(image_response.content)
+
+        # Send the image back to the user
+        message.reply_photo("downloaded_image.jpg")
+
     except Exception as e:
-        client.send_message(chat_id, f"Error: {str(e)}")
-
-
-@Client.on_message(filters.command("sendpost"))
-def send_post(client: Client, message: Message):
-    chat_id = message.chat.id
-
-    if message.reply_to_message and message.reply_to_message.text:
-        url = message.reply_to_message.text
-    else:
-        url = message.text.split(" ", 1)[1]
-
-    download_and_send_media(url, chat_id, client)
-
-
+        # Handle any exceptions and inform the user
+        message.reply_text(f"An error occurred: {str(e)}")
