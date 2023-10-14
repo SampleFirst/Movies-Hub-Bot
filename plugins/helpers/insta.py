8d11bc237142c1ebb428358009f2ad1bf5111ee5
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-14 17:44:53 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -2,29 +2,28 @@ from pyrogram import Client, filters
 import aiohttp
 from io import BytesIO
 
-
 # Command handler for downloading Instagram posts
 @Client.on_message(filters.command("download"))
 async def download_instagram_post(client, message):
+    # Check if the command includes an Instagram post link
     if len(message.command) > 1:
         instagram_link = message.command[1]
+
+        # Modify the link for downloading
         modified_link = instagram_link.replace("://www.instagram.com", "://www.ddinstagram.com")
 
         try:
+            # Use aiohttp for asynchronous HTTP requests
             async with aiohttp.ClientSession() as session:
                 async with session.get(modified_link) as response:
+                    # Check if the response is successful
                     if response.status == 200:
+                        # Read the content and send it as a photo
                         content = await response.read()
                         file_bytes = BytesIO(content)
-
-                        # Get file extension from response headers
-                        content_type = response.headers.get('content-type', '').split('/')
-                        file_extension = content_type[1] if len(content_type) == 2 else 'jpg'
-                        
-                        # Adjust the file name and extension
-                        file_bytes.name = f"downloaded_post.{file_extension.lower()}"
-                        
-                        await message.reply_photo(
+                        file_bytes.name = "downloaded_post.jpg"  # You can adjust the file name and extension
+                        await client.send_photo(
+                            chat_id=message.chat.id,
                             photo=file_bytes,
                             caption="Post downloaded successfully!"
                         )
@@ -34,5 +33,3 @@ async def download_instagram_post(client, message):
             await message.reply_text(f"Error: {str(e)}")
     else:
         await message.reply_text("Please provide an Instagram post link with the command.")
-
-
