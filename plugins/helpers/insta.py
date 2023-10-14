2d13b97797075f776eb6a48c6a4c13729afcb6a8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-14 16:33:35 +0530

    Update insta.py

--- plugins/helpers/insta.py
+++ plugins/helpers/insta.py
@@ -1,8 +1,9 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 import requests
 
 @Client.on_message(filters.command("download"))
-async def download_instagram_post(client, message):
+def download_instagram_post(client, message):
     # Get the Instagram post link from the command arguments
     if len(message.command) > 1:
         instagram_link = message.command[1]
@@ -13,20 +14,17 @@ async def download_instagram_post(client, message):
         # Download the Instagram post
         try:
             response = requests.get(modified_link)
-            
             # Process the response or save the content as needed
-            # For example, you can save it to an "Images" folder
-            file_name = "downloaded_post.jpg"
+            # For example, you can save it to a file
+            file_name = "downloaded_post.html"
             with open(file_name, "wb") as file:
                 file.write(response.content)
-            
-            # Send the downloaded file as an image
-            await client.send_photo(
-                chat_id=message.chat.id,
-                photo=open(file_name, "rb"),
+            # Send the downloaded file as a document
+            await message.reply_photo(
+                photo=file_name, 
                 caption="Post downloaded successfully!"
             )
         except Exception as e:
-            await message.reply_text(f"Error: {str(e)}")
+            message.reply_text(f"Error: {str(e)}")
     else:
-        await message.reply_text("Please provide an Instagram post link with the command.")
+        message.reply_text("Please provide an Instagram post link with the command.")
