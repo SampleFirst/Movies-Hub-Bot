2bc8a456f07eca11b818b12f73dfc97de949d27d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-15 23:07:06 +0530

    Create delete.py

---
+++ plugins/helpers/delete.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+from pyrogram.types import Message
+
+
+# Define the command handler
+@Client.on_message(filters.command("deletelast") & filters.group)
+await async def delete_last_100_messages(client: Client, message: Message):
+    # Get the chat ID and last 100 messages
+    chat_id = message.chat.id
+    messages = await client.get_chat_history(chat_id, limit=100)
+
+    # Extract message IDs
+    message_ids = [msg.message_id for msg in messages]
+
+    # Delete the last 100 messages
+    await client.delete_messages(chat_id, message_ids)
