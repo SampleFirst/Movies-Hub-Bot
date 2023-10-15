52f486246b110d22f24f1117779bc34e59729a7a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-16 00:19:14 +0530

    Update delete.py

--- plugins/helpers/delete.py
+++ plugins/helpers/delete.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.methods.messages import DeleteMessages  # Import the DeleteMessages class
 
-# Instantiate the DeleteMessages class
-delete_messages = DeleteMessages()
+
 
 # Define the command handler
 @Client.on_message(filters.command("deletelast"))
@@ -10,18 +9,19 @@ async def delete_last_messages(client, message):
     try:
         # Get the chat ID and last 100 messages
         chat_id = message.chat.id
-        messages = await client.get_chat_history(chat_id, limit=100)
+        messages = await client.get_chat_history(chat_id, limit=100).flatten()
 
         # Check if there are messages to delete
         if messages:
             # Extract message IDs
             message_ids = [msg.message_id for msg in messages]
 
-            # Delete the last 100 messages using the delete_messages method
-            await delete_messages.delete_messages(chat_id, message_ids)
+            # Delete the last 100 messages
+            await client.delete_messages(chat_id, message_ids)
             await message.reply_text("Successfully deleted last 100 messages.")
         else:
             await message.reply_text("No messages found to delete.")
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
+
 
