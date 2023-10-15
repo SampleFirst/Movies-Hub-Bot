ec03c243bb2d472df5cff694e1a17de9931fa4f2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-16 01:53:47 +0530

    Update delete.py

--- plugins/helpers/delete.py
+++ plugins/helpers/delete.py
@@ -1,26 +1,45 @@
-from pyrogram import Client, filters
+import asyncio
+from pyrogram import Client, filters, enums
+from info import ADMINS
 
-# Define the command handler
 @Client.on_message(filters.command("deletelast"))
 async def delete_last_messages(client, message):
-    try:
-        # Get the chat ID and last 100 messages
-        chat_id = message.chat.id
-        messages = []
+    if message.chat.type not in (enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL):
+        await message.reply_text("This command is only for groups or supergroups.")
+        return
 
-        async for msg in client.iter_messages(chat_id, limit=100):
-            messages.append(msg)
+    is_admin = message.from_user.id in ADMINS  # Check if the user is an admin
+    if not is_admin:
+        await message.reply_text("This command is only for admins.")
+        return
 
-        # Check if there are messages to delete
-        if messages:
-            # Extract message IDs
-            message_ids = [msg.message_id for msg in messages]
+    status_message = await message.reply_text("Deleting Messages...", quote=True)
+    await message.delete()
 
-            # Delete the last 100 messages
-            await client.delete_messages(chat_id, message_ids)
-            await message.reply_text("Successfully deleted last 100 messages.")
-        else:
-            await message.reply_text("No messages found to delete.")
-    except Exception as e:
-        await message.reply_text(f"An error occurred: {str(e)}")
+    message_ids = []
+    count_deletions = 0
+
+    if message.reply_to_message:
+        for a_s_message_id in range(message.reply_to_message.message_id, message.message_id):
+            message_ids.append(a_s_message_id)
+            if len(message_ids) == 100:
+                await client.delete_messages(
+                    chat_id=message.chat.id,
+                    message_ids=message_ids,
+                    revoke=True
+                )
+                count_deletions += len(message_ids)
+                message_ids = []
+
+        if len(message_ids) > 0:
+            await client.delete_messages(
+                chat_id=message.chat.id,
+                message_ids=message_ids,
+                revoke=True
+            )
+            count_deletions += len(message_ids)
+
+    await status_message.edit_text(f"Deleted {count_deletions} messages")
+    await asyncio.sleep(5)
+    await status_message.delete()
 
