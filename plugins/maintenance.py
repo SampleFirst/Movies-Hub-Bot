643d9292075d515e6bce9a4c114e9ec8f136bbd0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-16 17:27:21 +0530

    Create maintenance.py

---
+++ plugins/maintenance.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+maintenance_mode_enabled = False
+
+@Client.on_message(filters.command("maintenance") & filters.user(ADMINS))
+async def maintenance_mode(client: Client, message: Message):
+    global maintenance_mode_enabled
+    
+    if maintenance_mode_enabled:
+        maintenance_mode_enabled = False
+        await message.reply_text("Maintenance mode disabled.")
+    else:
+        maintenance_mode_enabled = True
+        await message.reply_text("Maintenance mode enabled.")
+        
+@Client.on_message(filters.text & filters.user(ADMINS))
+async def maintenance_mode_check(client: Client, message: Message):
+    if maintenance_mode_enabled:
+        await message.reply_text("Maintenance mode is enabled. Only admins can send messages.")
