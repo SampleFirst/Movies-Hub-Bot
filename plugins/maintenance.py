2a626bde937fd88457c47cbd2ab1b9a485c62a4d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-10-16 19:32:07 +0530

    Update maintenance.py

--- plugins/maintenance.py
+++ plugins/maintenance.py
@@ -2,16 +2,16 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from info import ADMINS
 
-maintenance_mode = False
+maintenance_mode_enable = False
 
 @Client.on_message(filters.command("maintenance") & filters.user(ADMINS))
 async def maintenance_mode_command(client, message):
-    global maintenance_mode
+    global maintenance_mode_enable
 
     keyboard = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton("Mode", callback_data="maintenance_mode"),
-            InlineKeyboardButton("OFF" if maintenance_mode else "ON", callback_data="maintenance_toggle")
+            InlineKeyboardButton("OFF" if maintenance_mode_enable else "ON", callback_data="maintenance_toggle")
         ]]
     )
 
@@ -20,27 +20,22 @@ async def maintenance_mode_command(client, message):
 
 @Client.on_callback_query(filters.regex(r'^maintenance_toggle') & filters.user(ADMINS))
 async def maintenance_toggle_callback(client, callback_query):
-    global maintenance_mode
+    global maintenance_mode_enable
 
-    maintenance_mode = not maintenance_mode  # Toggle maintenance mode
+    maintenance_mode_enable = not maintenance_mode_enable
 
     keyboard = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton("Mode", callback_data="maintenance_mode"),
-            InlineKeyboardButton("OFF" if maintenance_mode else "ON", callback_data="maintenance_toggle")
+            InlineKeyboardButton("OFF" if maintenance_mode_enable else "ON", callback_data="maintenance_toggle")
         ]]
     )
 
     await callback_query.edit_message_reply_markup(reply_markup=keyboard)
-    await callback_query.answer(f"Maintenance mode {'enabled' if maintenance_mode else 'disabled'}")
+    await callback_query.answer(f"Maintenance mode {'enabled' if maintenance_mode_enable else 'disabled'}")
 
 
-@Client.on_message(filters.text & filters.command & filters.user(ADMINS))
+@Client.on_message(filters.text | filters.command) & filters.user(ADMINS)
 async def maintenance_mode_check(client, message):
-    user_id = message.from_user.id
-
-    if maintenance_mode:
+    if maintenance_mode_enable:
         await message.reply_text("♻️ Maintenance mode is enabled.")
-    elif user_id in ADMINS:
-        pass
-        
