b244c0f2cc6b48401aec5fa1af50ab1072978634
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-11-03 01:29:31 +0530

    Update filechannel.py

--- plugins/filechannel.py
+++ plugins/filechannel.py
@@ -1,59 +1,34 @@
-import asyncio
-import re
 import math
-import logging
-from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from database.ia_filterdb import get_search_results
-from info import ADMINS, PROTECT_CONTENT
+from pyrogram import Client, filters
+from database.ia_filterdb import Media, get_all_files
+from info import ADMINS  # Make sure to define this constant in your info.py
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-# Function to handle the command and show results in buttons for all files
-async def show_all_files_command(client, message):
-    chat_id = message.chat.id
-    max_results = 10  # Adjust this value as needed
-    files, offset, total_results = await get_search_results(chat_id, '', max_results=max_results, offset=0, filter=True)
-
-    if not files:
-        await message.reply("No files found.")
-        return
-
-    pre = 'pmfilep' if PROTECT_CONTENT else 'pmfile'
-
+@Client.on_message(filters.command("getallmedia") & filters.user(ADMINS))
+async def send_all_media(client, message):
+    files, offset, total_results = await get_all_files()
+    
     btn = [
         [
-            InlineKeyboardButton(text=f"[{file.file_name}]", callback_data=f'{pre}#{file.file_id}')
+            InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'send#{file.file_id}')
         ] 
-        for file in files
+        for file in files 
     ]
-
+    
     if offset != "":
-        key = f"{message.message_id}"
-        temp.PM_BUTTONS[key] = ''
-        req = message.from_user.id if message.from_user else 0
         btn.append(
             [
-                InlineKeyboardButton(text=f"📄 Page 1/{math.ceil(total_results / max_results)}", callback_data="pages"),
-                InlineKeyboardButton(text="Next ➡️", callback_data=f"pmnext_{req}_{key}_{offset}")
+                InlineKeyboardButton(text=f"📄 Page 1/{math.ceil(total_results / 6)}", callback_data="pages"),
+                InlineKeyboardButton(text="Next ➡️", callback_data=f"pmnext_{offset}")
             ]
         )
     else:
         btn.append(
             [
-                InlineKeyboardButton(text=f"📄 Page 1/1", callback_data="pages")
+                InlineKeyboardButton(text="📄 Page 1/1", callback_data="pages")
             ]
         )
 
-        caption = f"Here are all available files:"
-        abc = await message.reply(caption, reply_markup=InlineKeyboardMarkup(btn))
-
-        await asyncio.sleep(IMDB_DELET_TIME)
-        await abc.delete()
-
-# Add a command handler for /showallfiles command
-@Client.on_message(filters.command("showallfiles") & filters.user(ADMINS))
-async def show_all_files(client, message):
-    await show_all_files_command(client, message)
-
+    cap = f"Here are the {total_results} media files found in the database."
+ 
+    abc = await message.reply_text(cap, quote=True, reply_markup=InlineKeyboardMarkup(btn))
