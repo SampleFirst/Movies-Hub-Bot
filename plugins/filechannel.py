b7e05f632bb5b95f8a9a275fbcc69780e28da098
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-11-03 21:03:20 +0530

    Update filechannel.py

--- plugins/filechannel.py
+++ plugins/filechannel.py
@@ -1,74 +1,61 @@
+import math
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_all_files
 from info import ADMINS
 
-@Client.on_message(filters.command("show_files") & filters.user(ADMINS))
-async def show_files_command(client, message):
+@Client.on_message(filters.command("getallmedia") & filters.user(ADMINS))
+async def send_all_media(client, message):
     try:
-        
-        # Get the list of files for the current page
         files, offset, total_results = await get_all_files()
 
-        if not files:
-            await message.reply("No files found.")
-            return
-
         btn = [
-            [
-                InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'send#{file.file_id}')
-            ]
+            [InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'send#{file.file_id}')]
             for file in files
         ]
 
-        if next_offset:
-            btn.append(InlineKeyboardButton("Next", callback_data=f"next_{next_offset}"))
-        if page > 0:
-            btn.append(InlineKeyboardButton("Previous", callback_data=f"prev_{page - 1}"))
+        if offset:
+            page_number = int(offset) // 5 + 1
+            btn.append([
+                InlineKeyboardButton(text=f"📄 Page {page_number}/{math.ceil(total_results / 5)}", callback_data="pages"),
+                InlineKeyboardButton(text="Next", callback_data=f"pmnext_{offset}")
+            ])
+        else:
+            btn.append([InlineKeyboardButton(text="📄 Page 1/1", callback_data="pages")])
 
         cap = f"Here are the {total_results} media files found in the database."
 
         abc = await message.reply_text(cap, quote=True, reply_markup=InlineKeyboardMarkup(btn))
-
-    except Exception as e:
-        print(e)
-        await message.reply("An error occurred while fetching files.")
-
-@Client.on_callback_query(filters.regex(r"^(prev|next)_\d+"))
-async def paginate_files(client, callback_query):
-    try:
-        data = callback_query.data.split("_")
-        action, offset = data[0], int(data[1])
-    
-
-        if action == "prev" and offset > 0:
-            page = offset - 1
-        elif action == "next":
-            page = offset
-    
-        files, next_offset, total_results = await get_all_files()
-    
-        if not files:
-            await callback_query.answer("No more files.")
-            return
-    
-        btn = [
-            [
-                InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'send#{file.file_id}')
-            ]
-            for file in files
-        ]
-    
-        if next_offset:
-            btn.append(InlineKeyboardButton("Next", callback_data=f"next_{next_offset}"))
-        if page > 0:
-            btn.append(InlineKeyboardButton("Previous", callback_data=f"prev_{page - 1}"))
-    
-        await callback_query.edit_message_reply_markup(
-            reply_markup=InlineKeyboardMarkup(btn)
-        )
-        await callback_query.answer()
     except Exception as e:
         # Handle any exceptions here
         print(f"An error occurred: {str(e)}")
-        
+
+@Client.on_callback_query(filters.regex(r'^pmnext_'))
+async def next_page_button(client, query: CallbackQuery):
+    try:
+        offset = query.data.split("_")[1]
+        files, new_offset, total_results = await get_all_files(max_results=5, offset=int(offset))
+        page_number = int(offset) // 5 + 1
+
+        btn = [
+            [InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'send#{file.file_id}')]
+            for file in files
+        ]
+
+        if new_offset:
+            page_number = int(new_offset) // 5 + 1
+            btn.append([
+                InlineKeyboardButton(text=f"📄 Page {page_number}/{math.ceil(total_results / 5)}", callback_data="pages"),
+                InlineKeyboardButton(text="Next", callback_data=f"pmnext_{new_offset}")
+            ])
+        else:
+            btn.append([InlineKeyboardButton(text=f"📄 Page {page_number}/{math.ceil(total_results / 5)}", callback_data="pages")])
+
+        await query.edit_message_text(
+            text=query.message.text.markdown,
+            reply_markup=InlineKeyboardMarkup(btn)
+        )
+    except Exception as e:
+        # Handle any exceptions here
+        print(f"An error occurred: {str(e)}")
