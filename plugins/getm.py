24ee6d97ac40f6f704af7fee8e81cecaa50fd011
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-11-05 21:56:03 +0530

    Update getm.py

--- plugins/getm.py
+++ plugins/getm.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import FILE_DB_CHANNEL  # Assuming you have defined FILE_CHANNEL in info.py
+from info import FILE_DB_CHANNEL
 from database.ia_filterdb import Media, get_all_saved_media
 
 
@@ -7,7 +7,6 @@ from database.ia_filterdb import Media, get_all_saved_media
 @Client.on_message(filters.command("send_all_media"))
 async def send_all_media_command(client, message):
     try:
-        chat_id = FILE_DB_CHANNEL  # Use the FILE_CHANNEL where you want to send the media
         files = await get_all_saved_media()
 
         if not files:
@@ -15,13 +14,16 @@ async def send_all_media_command(client, message):
         else:
             for file in files:
                 try:
-                    # Send each file to the specified channel
-                    # You can customize this part based on your requirements
-                    await client.send_cached_media(chat_id, file.file_id, caption=file.caption)
+                    await bot.send_cached_media(
+                        chat_id=FILE_DB_CHANNEL,
+                        file_id=file.file_id,
+                        caption=file.caption,
+                    )
                 except Exception as e:
                     print(f"Error sending media: {str(e)}")
-                    continue  # Continue with the next file if there's an error
-
+                    continue
+                    
         await message.reply("All saved media files have been sent to the channel.")
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
+        
