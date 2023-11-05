05af2afe74a54b59981c15db4e5809036b197ab0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-11-05 23:54:34 +0530

    Update mystats.py

--- plugins/mystats.py
+++ plugins/mystats.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from info import DATABASE_NAME, FILE_CHANNEL
-from database.ia_filterdb import Media
+from info import MAX_BTN
+from database.ia_filterdb import Media, get_files_from_channel
 
 @Client.on_message(filters.command("mystats"))
 async def get_stats(_, message):
@@ -11,7 +11,6 @@ async def get_stats(_, message):
 
     response_text = f"Total Documents: {total_documents}\nTotal Videos: {total_videos}\nTotal Audio Files: {total_audio}"
 
-    # Create inline keyboard with 4 buttons
     keyboard = InlineKeyboardMarkup(
         [
             [
@@ -29,3 +28,48 @@ async def get_stats(_, message):
         response_text,
         reply_markup=keyboard,
     )
+
+@Client.on_callback_query(filters.regex(r"send_documents"))
+async def send_documents_button(_, callback_query):
+    try:
+        files = await get_files_from_channel("document", MAX_BTN)
+        if files:
+            for file in files:
+                await callback_query.message.reply_document(file.file_id)
+            await callback_query.answer(f"Sent {len(files)} Documents")
+        else:
+            await callback_query.answer("No Documents found.")
+    except Exception as e:
+        await callback_query.answer(f"Error: {str(e)}")
+
+@Client.on_callback_query(filters.regex(r"send_videos"))
+async def send_videos_button(_, callback_query):
+    try:
+        files = await get_files_from_channel("video", MAX_BTN)
+        if files:
+            for file in files:
+                await callback_query.message.reply_video(file.file_id)
+            await callback_query.answer(f"Sent {len(files)} Videos")
+        else:
+            await callback_query.answer("No Videos found.")
+    except Exception as e:
+        await callback_query.answer(f"Error: {str(e)}")
+
+@Client.on_callback_query(filters.regex(r"send_audios"))
+async def send_audios_button(_, callback_query):
+    try:
+        files = await get_files_from_channel("audio", MAX_BTN)
+        if files:
+            for file in files:
+                await callback_query.message.reply_audio(file.file_id)
+            await callback_query.answer(f"Sent {len(files)} Audios")
+        else:
+            await callback_query.answer("No Audios found.")
+    except Exception as e:
+        await callback_query.answer(f"Error: {str(e)}")
+
+@Client.on_callback_query(filters.regex(r"cancel_send"))
+async def cancel_send_button(_, callback_query):
+    await callback_query.answer("Canceling Send...")
+
+    
