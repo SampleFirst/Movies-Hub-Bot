1168ec4e997ed89dad4965c72fb13f01313a5742
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-11-07 23:49:26 +0530

    Update mystats.py

--- plugins/mystats.py
+++ plugins/mystats.py
@@ -1,135 +1,36 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import FloodWait
-from info import FILE_DB_CHANNEL
+from pyrogram.types import Message
 from database.ia_filterdb import Media, get_files_from_channel
 import asyncio
-import logging
+from info import ADMINS, FILE_DB_CHANNEL 
 
-MAX_BUTTON = 5
 
-# Enable logging
-logging.basicConfig(level=logging.INFO)
+# Command to send all saved media in the database to the FILE_CHANNEL
+@Client.on_message(filters.command("send_all_media"))
+async def send_all_media_to_channel(_, message: Message):
 
-async def send_media_files_in_batches(bot, files, file_type, batch_size, chat_id, callback_query, batch_index=0):
-    try:
-        total_files = len(files)
-        sent_count = 0
-        corrupted_count = 0
+    # Check if the user has the necessary permissions to run this command
+    if user_id in ADMINS:
+        # Get all saved media from the database
+        saved_media, _ = await get_search_results(None, '', file_type=None, max_results=1000000, offset=0, filter=False)
 
-        for i in range(batch_index, total_files, batch_size):
-            batch = files[i:i + batch_size]
-            for file in batch:
-                try:
-                    if file_type == "document":
-                        await bot.send_document(chat_id=chat_id, document=file.file_id)
-                    elif file_type == "video":
-                        await bot.send_video(chat_id=chat_id, video=file.file_id)
-                    elif file_type == "audio":
-                        await bot.send_audio(chat_id=chat_id, audio=file.file_id)
+        # Send each media to the FILE_CHANNEL
+        for media in saved_media:
+            file_id = media.file_id
+            caption = media.caption
+            file_name = media.file_name
+            file_type = media.file_type
+            mime_type = media.mime_type
 
-                    sent_count += 1
+            # Send the media to the channel
+            try:
+                await message.forward(FILE_DB_CHANNEL)
+                await asyncio.sleep(1)  # Add a delay between sending each media
+            except Exception as e:
+                print(f"Error sending media: {e}")
 
-                    # Update the message with live statistics
-                    message = (
-                        f"Total Files: {total_files}\n"
-                        f"Sent Files: {sent_count}\n"
-                        f"Corrupted Files: {corrupted_count}\n"
-                        f"Batches Sent: {i // batch_size + 1}"
-                    )
-
-                    await callback_query.edit_message_text(message)
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-                except Exception as e:
-                    corrupted_count += 1
-
-            await asyncio.sleep(5)
-            return 
-
-        return total_files, sent_count, corrupted_count, i
-    except Exception as e:
-        return str(e)
-
-@Client.on_message(filters.command("mystats"))
-async def get_stats(_, message):
-    try:
-        total_documents = await Media.count_documents()
-        total_videos = await Media.count_documents({"file_type": "video"})
-        total_audio = await Media.count_documents({"file_type": "audio"})
-
-        response_text = (
-            f"Total Documents: {total_documents}\n"
-            f"Total Videos: {total_videos}\n"
-            f"Total Audio Files: {total_audio}"
-        )
-
-        keyboard = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Send Documents", callback_data="send_documents"),
-                    InlineKeyboardButton("Send Videos", callback_data="send_videos"),
-                ],
-                [
-                    InlineKeyboardButton("Send Audios", callback_data="send_audios"),
-                    InlineKeyboardButton("Cancel Send", callback_data="cancel_send"),
-                ],
-            ]
-        )
-
-        await message.reply(response_text, reply_markup=keyboard)
-
-    except Exception as e:
-        logging.error(f"Error in get_stats: {str(e)}")
-
-async def send_media_files_button(bot, callback_query, file_type, description):
-    try:
-        batch_index = 0
-        files = await get_files_from_channel(file_type, MAX_BUTTON, batch_index)
-        if not files:
-            await callback_query.message.reply_text(f"No {description} found.")
-            return
-
-        total_files = 0
-        sent_count = 0
-        corrupted_count = 0
-
-        while files:
-            total_files += len(files)
-
-            total_files, sent_count, corrupted_count, batch_index = await send_media_files_in_batches(
-                bot, files, file_type, MAX_BUTTON, FILE_DB_CHANNEL, callback_query, batch_index
-            )
-            
-            files = await get_files_from_channel(file_type, MAX_BUTTON, batch_index)  # Get the next batch of files
-
-        message = (
-            f"Total {description}: {total_files}\n"
-            f"Sent {description}: {sent_count}\n"
-            f"Corrupted {description}: {corrupted_count}\n"
-            f"Batches Sent: {batch_index // MAX_BUTTON}"
-        )
-        await callback_query.message.reply_text(message)
-    except Exception as e:
-        logging.error(f"Error in send_{file_type}_button: {str(e)}")
-
-@Client.on_callback_query(filters.regex(r"send_documents"))
-async def send_documents_button(bot, callback_query):
-    await send_media_files_button(bot, callback_query, "document", "Documents")
-
-@Client.on_callback_query(filters.regex(r"send_videos"))
-async def send_videos_button(bot, callback_query):
-    await send_media_files_button(bot, callback_query, "video", "Videos")
-
-@Client.on_callback_query(filters.regex(r"send_audios"))
-async def send_audios_button(bot, callback_query):
-    await send_media_files_button(bot, callback_query, "audio", "Audios")
-
-@Client.on_callback_query(filters.regex(r"cancel_send"))
-async def cancel_send_button(bot, callback_query):
-    try:
-        await callback_query.edit_message_text("Canceling Send...")
-        # Add your cancel logic here if needed
-    except Exception as e:
-        logging.error(f"Error in cancel_send_button: {str(e)}")
+        # Send a status message to the user
+        await message.reply("All saved media sent to FILE_CHANNEL.")
+    else:
+        await message.reply("You do not have permission to run this command.")
 
